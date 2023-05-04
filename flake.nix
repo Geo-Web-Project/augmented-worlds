3b4e82ebc8a9ec3ecc5c8cb7da8cfa7173a2cb57
Cody Hatfield <cody.hatfield@me.com>
2023-05-04 13:22:03 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A basic flake with a shell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            exa
+            fd
+            rust
+            wasmer
+            wapm-cli
+          ];
+
+          shellHook = ''
+            alias ls=exa
+            alias find=fd
+          '';
+        };
+      }
+    );
+}
